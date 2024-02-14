12a059bf48c2cb8bc130367eef7e9f37f5b1ee7d
mirac025 <mrc.aksy01@gmail.com>
2024-02-14 10:18:03 +0100

    deneme3

--- src/Gün01/Merhaba1.java
+++ src/Gün01/Merhaba1.java
@@ -7,7 +7,7 @@ public class Merhaba1 {
     public static void main(String[] args) {
 
         System.out.println("merhaba1");
-        System.out.println("merhaba2");
+        System.out.println("merhaba6");
         System.out.println("merhaba3");
 
     }
