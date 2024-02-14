3bf3df4b0d155e93b4317a951d0fbeac711edffb
mirac025 <mrc.aksy01@gmail.com>
2024-02-14 09:45:51 +0100

    deneme1

---
+++ src/Gün01/Merhaba1.java
@@ -0,0 +1,13 @@
+package Gün01;
+
+import java.util.Scanner;
+
+public class Merhaba1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("merhaba1");
+        System.out.println("merhaba2");
+
+    }
+}
